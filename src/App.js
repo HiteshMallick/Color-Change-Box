247e17626567c91a1dadc9d4d7b9dc97fc639c0a
HiteshKumarMallick <officialhitesh1227@gmail.com>
2023-09-04 21:08:55 +0530

    color change Box
    U can write any color inside input box and press change button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [color, setColor] = useState('')
+  const refEle = useRef()
+
+  function handelChange(){
+    refEle.current.style.backgroundColor = color
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Color Change Box</h1>
+      <div className="main">
+        <div ref={refEle} className="colorChange"></div>
+        <input type="text" placeholder="Color-Name" value={color} onChange={(e)=>setColor(e.target.value)}/>
+        <button onClick={handelChange}>CHANGE</button>
+      </div>
+    </>
   );
 }
 
